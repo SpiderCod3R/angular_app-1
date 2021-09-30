3a3c070fdb132276093d5aeb115c20f53a01c9a7
THUNDER MASTER <thndrmaster@gmail.com>
2021-09-30 14:57:25 -0300

    Criando o Mock de Frases Para teste

---
+++ src/app/components/painel/frases.mock.ts
@@ -0,0 +1,11 @@
+import { Frase } from "src/app/shared/frase.model";
+
+// Array<Frase>
+export const FRASE: Frase[] = [
+  { fraseEng: 'I love to Learn', frasePtBr: 'Eu amo aprender' },
+  { fraseEng: 'It’s a beautiful day, isn’t? ', frasePtBr: 'Está um dia bonito hoje, não?' },
+  { fraseEng: 'I slept well today', frasePtBr: 'Eu dormi bem hoje' },
+  { fraseEng: 'Nice to meet you', frasePtBr: 'Prazer em conhecê-lo' },
+  { fraseEng: 'I like eat Hamburguer', frasePtBr: 'Eu gosto de comer Hamburguer' },
+  { fraseEng: 'Anything they can do i can do better', frasePtBr: 'Tudo que eles fazem, eu posso fazer melhor' },
+]
--- src/app/components/painel/painel.component.ts
+++ src/app/components/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from 'src/app/shared/frase.model';
+import { FRASE } from './frases.mock';
 
 @Component({
   selector: 'app-painel',
@@ -6,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
-
-  constructor() { }
+  public frases: Frase[] =FRASE
+  
+  constructor() { console.log(this.frases) }
 
   ngOnInit(): void {
   }
