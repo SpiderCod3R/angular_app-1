642ec46281321ecfa251ccccff95a9d084574ec8
THUNDER MASTER <thndrmaster@gmail.com>
2021-10-04 13:21:14 -0300

    112. App1 - @Input (property binding entre o componente painel e progresso)

--- src/app/components/painel/frases.mock.ts
+++ src/app/components/painel/frases.mock.ts
@@ -5,7 +5,5 @@ export const FRASE: Frase[] = [
   { fraseEng: 'I love to Learn', frasePtBr: 'Eu amo aprender' },
   { fraseEng: 'It’s a beautiful day, isn’t? ', frasePtBr: 'Está um dia bonito hoje, não?' },
   { fraseEng: 'I slept well today', frasePtBr: 'Eu dormi bem hoje' },
-  { fraseEng: 'Nice to meet you', frasePtBr: 'Prazer em conhecê-lo' },
-  { fraseEng: 'I like eat Hamburguer', frasePtBr: 'Eu gosto de comer Hamburguer' },
-  { fraseEng: 'Anything they can do i can do better', frasePtBr: 'Tudo que eles fazem, eu posso fazer melhor' },
+  { fraseEng: 'Anything they can do i can do better', frasePtBr: 'Tudo que eles fazem eu posso fazer melhor' },
 ]
